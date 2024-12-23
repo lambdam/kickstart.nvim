b70433a19f476d457573b6309f632c4f383347d2
Damien Ragoucy <damien@lambdam.com>
2024-12-23 10:58:58 +0100

    Update lock file

--- init.lua
+++ init.lua
@@ -937,7 +937,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'fennel' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -983,6 +983,13 @@ require('lazy').setup({
     },
     { 'UtkarshVerma/molokai.nvim' },
     { 'RRethy/base16-nvim' },
+    {
+      'windwp/nvim-autopairs',
+      event = 'InsertEnter',
+      config = true,
+      -- use opts = {} for passing setup options
+      -- this is equivalent to setup({}) function
+    },
     -- END dam custom code
   },
 
