971b82b17fd0256d7dbe65a27bd1c40b50ff9d8b
Damien Ragoucy <damien.ragoucy@mckay-brothers.com>
2025-05-19 14:07:32 +0200

    Disable cmp for conjure

--- init.lua
+++ init.lua
@@ -891,18 +891,18 @@ require('lazy').setup({
       end,
 
       -- Optional cmp-conjure integration
-      dependencies = { 'PaterJason/cmp-conjure' },
+      -- dependencies = { 'PaterJason/cmp-conjure' },
     },
-    {
-      'PaterJason/cmp-conjure',
-      lazy = true,
-      config = function()
-        local cmp = require 'cmp'
-        local config = cmp.get_config()
-        table.insert(config.sources, { name = 'conjure' })
-        return cmp.setup(config)
-      end,
-    },
+    -- {
+    --   'PaterJason/cmp-conjure',
+    --   lazy = true,
+    --   config = function()
+    --     local cmp = require 'cmp'
+    --     local config = cmp.get_config()
+    --     table.insert(config.sources, { name = 'conjure' })
+    --     return cmp.setup(config)
+    --   end,
+    -- },
     { 'UtkarshVerma/molokai.nvim' },
     { 'RRethy/base16-nvim' },
     {
