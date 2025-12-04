f4e63bd31d9fdd0e2745fd45e6be6b0122ae9488
Damien Ragoucy <damien.ragoucy@mckay-brothers.com>
2025-12-04 11:34:43 +0100

    Add nvim neotree

--- init.lua
+++ init.lua
@@ -936,6 +936,16 @@ require('lazy').setup({
       'terrastruct/d2-vim',
       ft = { 'd2' },
     },
+    {
+      'nvim-neo-tree/neo-tree.nvim',
+      branch = 'v3.x',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'MunifTanjim/nui.nvim',
+        'nvim-tree/nvim-web-devicons', -- optional, but recommended
+      },
+      lazy = false, -- neo-tree will lazily load itself
+    },
     -- END dam custom code
   },
 
