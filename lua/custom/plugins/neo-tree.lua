d487fc73fa52eeb873997e35601ab46857d0e2ba
Damien Ragoucy <damien.ragoucy@mckay-brothers.com>
2026-06-03 13:02:55 +0200

    Update neo-tree config

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -7,6 +7,14 @@ local function toggle_width()
   vim.api.nvim_win_set_width(win, current ~= default_width and default_width or wide)
 end
 
+-- Live grep in the folder under the cursor in the tree
+local function grep_in_node(state)
+  local node = state.tree:get_node()
+  -- For a file, use its parent directory; for a directory, use the directory itself
+  local dir = node.type == 'directory' and node.path or vim.fn.fnamemodify(node.path, ':h')
+  require('telescope.builtin').live_grep { search_dirs = { dir } }
+end
+
 require('neo-tree').setup {
   window = {
     width = default_width,
@@ -18,6 +26,8 @@ require('neo-tree').setup {
         ['<'] = function() vim.cmd('vertical resize -5') end,
         ['>'] = function() vim.cmd('vertical resize +5') end,
         ['<Tab>'] = toggle_width,
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        ['gs'] = { grep_in_node, desc = '[G]rep in folder' },
       },
     },
   },
@@ -32,3 +42,12 @@ end
 update_background()
 
 vim.api.nvim_create_autocmd('ColorScheme', { callback = update_background })
+
+-- Live grep scoped to a prompted subfolder (defaults to the current file's folder)
+vim.keymap.set('n', '<leader>sG', function()
+  vim.ui.input({ prompt = 'Subfolder: ', completion = 'dir', default = vim.fn.expand '%:h' }, function(dir)
+    if dir and dir ~= '' then
+      require('telescope.builtin').live_grep { search_dirs = { dir } }
+    end
+  end)
+end, { desc = '[S]earch by [G]rep in folder' })
