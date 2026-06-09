2ee69aa920353aeb6d755d9edb795612304c5954
Damien Ragoucy <damien.ragoucy@mckay-brothers.com>
2026-06-09 17:02:03 +0200

    Activate exrc (per project .nvim.lua files)

--- init.lua
+++ init.lua
@@ -185,6 +185,13 @@ do
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
   vim.o.confirm = true
+
+  -- Load project-local config files (`.nvim.lua` / `.nvimrc` / `.exrc`) from the
+  -- directory Neovim is launched in. Neovim prompts to `:trust` them on first
+  -- load. Used here so Askama projects can map `.html` -> the `askama` filetype.
+  -- See `:help 'exrc'`
+  vim.o.exrc = true
+
 end
 
 -- ============================================================
