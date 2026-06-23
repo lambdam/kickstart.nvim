b7780c28281f172981574639f3e702e7336dfbec
Damien Ragoucy <damien.ragoucy@mckay-brothers.com>
2026-06-23 12:21:00 +0200

    Activate autopairing

--- init.lua
+++ init.lua
@@ -1040,7 +1040,7 @@ do
   require 'kickstart.plugins.debug'
   -- require 'kickstart.plugins.indent_line'
   -- require 'kickstart.plugins.lint'
-  -- require 'kickstart.plugins.autopairs'
+  require 'kickstart.plugins.autopairs'
   require 'kickstart.plugins.neo-tree'
 
   -- NOTE: You can add your own plugins, configuration, etc. in `lua/custom/plugins/*.lua`.
